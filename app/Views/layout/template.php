c5006d940ae01ee9c7b64fe76264f7aba892189d
yukikaru098 <dzakiabiyyu01@gmail.com>
2024-09-09 13:54:59 +0700

    lelang

---
+++ app/Views/layout/template.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Situs lelang</title>
+    <!-- Bootstrap CSS -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css" rel="stylesheet">
+    <style>
+
+    </style>
+
+</head>
+
+
+<?= $this->include('layout/navbar'); ?>
+
+<?= $this->renderSection('content'); ?>
+
+<footer class="bg-primary">
+    <div class="bg-success" style="height: 1px;"></div>
+    <div class="d-flex flex-column justify-content-between gap-4 p-4 text-white flex-md-row px-md-4 px-lg-5">
+        <div class="d-flex flex-column gap-4">
+            <!-- Logo placeholder -->
+            <a href="/"><img src="#" alt="Logo" class="h-16 w-40"></a>
+            <div class="d-flex gap-2">
+                <span class="pi pi-map-marker text-lg text-white"></span>
+                <span class="text-xs md:text-sm">Cibarusah bagian selatan barat daya </span>
+            </div>
+        </div>
+        <div class="d-flex flex-wrap justify-content-between gap-4 text-white">
+            <div>
+                <p class="mb-2 font-bold">Layanan</p>
+                <div class="d-flex flex-column">
+                    <a class="text-light text-decoration-underline" href="#">Daftar Balai Lelang</a>
+                    <a class="text-light text-decoration-underline" href="#">Daftar PL Kelas II</a>
+                    <a class="text-light text-decoration-underline" href="#">Ijin Operasional Balai Lelang</a>
+                    <a class="text-light text-decoration-underline" href="#">Pengangkatan PL Kelas II</a>
+                    <a class="text-light text-decoration-underline" href="#">Ijin Pindah Wilayah Jabatan PL
+                        Kelas
+                        II</a>
+                    <a class="text-light text-decoration-underline" href="#">Laporan Kinerja</a>
+                    <a class="text-light text-decoration-underline" href="#">Ijin Perpanjangan Masa Jabatan PL
+                        Kelas
+                        II</a>
+                </div>
+            </div>
+            <div>
+                <p class="mb-2 font-bold">Hubungi Kami</p>
+                <div class="d-flex flex-column gap-2">
+                    <div class="d-flex align-items-center gap-2">
+                        <span class="bi bi-telephone"></span>
+                        <span>Call Center 150-991</span>
+                    </div>
+                    <div class="d-flex align-items-center gap-2">
+                        <span class="bi bi-envelope"></span>
+                        <a href="mailto:halodjkn@kemenkeu.go.id"
+                            class="text-white text-decoration-none">halodjkn@kemenkeu.go.id</a>
+                    </div>
+                    <div class="d-flex align-items-center gap-2">
+                        <span class="bi bi-link"></span>
+                        <a href="https://www.djkn.kemenkeu.go.id"
+                            class="text-white text-decoration-none">https://djkn.kemenkeu.go.id</a>
+                    </div>
+                </div>
+                <div class="mt-2 d-flex gap-2">
+                    <a href="https://play.google.com/store/apps" target="_blank"
+                        class="text-white text-decoration-none">Play Store</a>
+                    <a href="https://www.apple.com/id/app-store/" target="_blank"
+                        class="text-white text-decoration-none">App Store</a>
+                </div>
+            </div>
+            <div>
+                <p class="mb-2 font-bold">Tautan Mitra</p>
+                <div class="d-flex gap-4">
+                    <div class="h-8 w-16 bg-light"></div> <!-- Placeholder for partner logo -->
+                    <div class="h-8 w-16 bg-light"></div> <!-- Placeholder for partner logo -->
+                    <div class="h-8 w-16 bg-light"></div> <!-- Placeholder for partner logo -->
+                    <div class="h-8 w-16 bg-light"></div> <!-- Placeholder for partner logo -->
+                    <div class="h-8 w-16 bg-light"></div> <!-- Placeholder for partner logo -->
+                    <div class="h-8 w-16 bg-light"></div> <!-- Placeholder for partner logo -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-white" style="height: 1px;"></div>
+    <div class="d-flex justify-content-between bg-primary text-white px-2 py-3">
+        <div>©Copyright 2024. Your Company Name.</div>
+        <div class="h-8 w-16 bg-light"></div> <!-- Placeholder for logo -->
+    </div>
+</footer>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+</body>
+
+</html>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,160 @@
+<?= $this->extend('layout/template'); ?>
+
+
+<?= $this->section('content'); ?>
+
+<body>
+
+
+    <div class="responsive-div">
+        <!-- <img src="https://via.placeholder.com/1200x400" alt="Responsive Image" class="img-fluid responsive-image"> -->
+        <h3> Selamat Datang Di Dunia Lelang</h3>
+    </div>
+
+
+
+    <!-- Content -->
+    <section class="container">
+        <div class="section-header">
+            <h3>Kategori Objek Lelang</h3>
+        </div>
+        <div class="scroll-container">
+            <div class="box-container">
+                <div class="box">
+                    <a href="#">Box 1</a>
+                </div>
+                <div class="box">
+                    <a href="#">Box 2</a>
+                </div>
+                <div class="box">
+                    <a href="#">Box 3</a>
+                </div>
+                <div class="box">
+                    <a href="#">Box 4</a>
+                </div>
+                <div class="box">
+                    <a href="#">Box 5</a>
+                </div>
+                <div class="box">
+                    <a href="#">Box 6</a>
+                </div>
+                <div class="box">
+                    <a href="#">Box 7</a>
+                </div>
+                <div class="box">
+                    <a href="#">Box 8</a>
+                </div>
+                <div class="box">
+                    <a href="#">Box 9</a>
+                </div>
+                <div class="box">
+                    <a href="#">Box 10</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container p-4">
+        <h3 style="padding: 10px;">Katalog Lelang</h3>
+        <div class="row pt-10 ">
+            <!-- Card 1 -->
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Image 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title 1</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                            of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card 2 -->
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Image 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title 2</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                            of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card 3 -->
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Image 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title 2</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                            of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card 4 -->
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Image 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title 2</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                            of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card 5 -->
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Image 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title 2</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                            of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card 6 -->
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Image 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title 2</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                            of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card 7 -->
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Image 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title 2</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                            of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card 8 -->
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Image 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title 2</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                            of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Add more cards as needed -->
+        </div>
+    </section>
+
+    <?= $this->endSection(); ?>
